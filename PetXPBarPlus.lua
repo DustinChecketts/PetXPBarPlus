168fb1c0d918f428fe02208588d83297c0c91b55
Dustin Checketts <8584623+DustinChecketts@users.noreply.github.com>
2023-08-20 20:52:50 -0400

    Initial Commit

---
+++ PetXPBarPlus.lua
@@ -0,0 +1,71 @@
+-- World of Warcraft: Classic Era (1.14.4) user-interface addon that displays Hunters' pet experience and level in the PetFrame.
+
+-- Frame
+local f = CreateFrame("Frame", nil, UIParent)
+f:SetSize(50, 10)
+f:ClearAllPoints()
+f:SetPoint("CENTER", PetFrame.portrait, "CENTER", -3, -22)
+
+-- Status Bar
+f.bar = CreateFrame("StatusBar", nil, f)
+f.bar:SetWidth(46)
+f.bar:SetHeight(8)
+f.bar:SetPoint("LEFT", f, "LEFT", 2, 0)
+f.bar:SetMinMaxValues(0, 100)
+f.bar:SetStatusBarTexture("Interface\\TargetingFrame\\UI-StatusBar")
+f.bar:SetStatusBarColor(25/255, 125/255, 255/255)
+
+-- Status Bar Border
+f.bar.border = f.bar:CreateTexture("PetXPBarBorder", "OVERLAY")
+f.bar.border:SetTexture("Interface\\Tooltips\\UI-StatusBar-Border")
+f.bar.border:SetAllPoints(f)
+
+-- Pet Level Text
+f.bar.text = f.bar:CreateFontString("PetXPBarText", "OVERLAY", "GameFontHighlight")
+f.bar.text:SetPoint("LEFT", PetFrame.portrait, "LEFT", -10, -12)
+f.bar.text:SetJustifyH("CENTER")
+f.bar.text:SetJustifyV("CENTER")
+
+-- Update Status Bar with Pet XP
+function updateBar()
+    local currXP, nextXP = GetPetExperience()
+    print("Pet Xp: " .. string.format("%2.2f", (currXP / nextXP) * 100) .. "%")
+    f.bar:SetValue((currXP / nextXP) * 100)
+end
+
+-- Update Pet Level Text with pet level
+function updateText()
+    local level = UnitLevel("pet")
+    print("Pet Level: " .. level)
+    f.bar.text:SetText(level)
+end
+
+-- Update Pet Experience Progress
+local initer = CreateFrame("Frame")
+initer:RegisterEvent("UNIT_PET_EXPERIENCE")
+initer:SetScript("OnEvent", function(self, event)
+    updateBar()
+    updateText()
+end)
+
+-- Event Handling
+local reloader = CreateFrame("Frame")
+reloader:RegisterEvent("UNIT_PET")
+reloader:SetScript("OnEvent", function(self, event)
+    hunterPetActive()
+end)
+
+-- Show/Hide when Pet is Present/Not Present
+function hunterPetActive()
+    local hasUI, isHunterPet = HasPetUI()
+    f:Hide()
+    if hasUI then
+        if isHunterPet then
+            updateBar()
+            updateText()
+            f:Show()
+        end
+    end
+end
+
+hunterPetActive()
---
+++ README.md
@@ -0,0 +1,24 @@
+# Pet XP Bar Plus
+A World of Warcraft: Classic 1.14.4+ user interface addon that displays a mini XP bar and current level of a Hunter's active pet in the default PlayerFrame PetFrame.
+
+You don't have to open your Pet tab any longer to check the level of your pet and how close they are to leveling. Pet XP Bar Plus is a simple addon that displays a mini XP bar for the Hunter's current pet, as well as the pet's level, near the PetFrame portrait in the default UI. When no pet is active, this frame is hidden. This addon is ideal for addon minimalists who prefer the iconic Classic user-interface with some handy, modern elements.
+
+<h4>Features</h4>
+
+- A mini experience bar below the PetFrame.portrait
+- Text displaying the current level of your active pet within the PetFrame.portrait
+- Doesn't interfere or overlap with pet buff, debuffs, or food
+- Appears like it could belong in the native UI
+- Scales properly with UI Scaling enabled
+
+<h4>Acknowledgements</h4>
+
+- [PetXPBar](https://www.curseforge.com/wow/addons/petxpbar) by [weryis](https://legacy.curseforge.com/members/weryis/projects)
+- [PetXP](https://www.curseforge.com/wow/addons/petxp) by [waggz81](https://legacy.curseforge.com/members/waggz81/projects)
+
+<h4>Known Issues / To-Do</h4>
+
+- Add slash commands to enable/disable the addon
+- Add options menu to allow minor customizations such as bar color, height, width, and font
+
+Updated, uploaded, and maintained (at least for now) by [StormtrooperTK421](https://discordapp.com/users/237746068844969994) on [GitHub](https://github.com/DustinChecketts/PetXPBarPlus). Please submit issues and I'll do my best to troubleshoot, replicate, and resolve issues as my limited abilities allow.
